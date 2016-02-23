591d4070e836c4a4e90a9fd33fca60fbedbcaade
markpurser <mail@markpurser.co.uk>
2016-02-23 20:30:31 +0000

    License additions

--- src/ore.js
+++ src/ore.js
@@ -1,7 +1,11 @@
 /*
 * ORE - Online Roguelike Engine
 * Copyright (C) 2016  Mark Purser
-* 
+*
+* Tile rendering based on yendor.ts
+* https://github.com/jice-nospam/yendor.ts
+* Copyright (c) 2014 Jice
+*
 * Permission is hereby granted, free of charge, to any person
 * obtaining a copy of this software and associated documentation
 * files (the "Software"), to deal in the Software without
